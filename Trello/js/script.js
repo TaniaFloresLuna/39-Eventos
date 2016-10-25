0bb2932eaf8ca6f8e9a2df6727f1d6e063c46892
TaniaFloresLuna <tanialuna08920@gmail.com>
2016-10-25 16:46:41 -0500

    Ejercicio Trello con mejores funciones y eventos. Básico de javascript DOM II

--- Trello/js/script.js
+++ Trello/js/script.js
@@ -1,5 +1,3 @@
-var primeraCaja = document.getElementById("primeraCaja");
-var tarjeta = document.getElementById("tarjeta");
 
 /*
 tarjeta.onkeypress=function(e){
@@ -11,43 +9,81 @@ tarjeta.onkeypress=function(e){
   	}
 }
 */
+//variables universales.
+var primeraCaja = document.getElementById("primeraCaja");
+var tarjeta = document.getElementById("tarjeta");
+var inputBoton = document.createElement('input');
+var boton = document.createElement("button");
 
+//función para el input
+function hacerInput(){
+
+	inputBoton.setAttribute('type', 'input');
+	var contenido = document.createElement('li');
+	contenido.appendChild(inputBoton);
+	document.body.appendChild(contenido);
+
+}
+hacerInput();
+
+//Función crear del botón
+function hacerBoton(){
+
+boton.setAttribute("class", "btn");
+boton.appendChild(document.createTextNode("Guardar"));
+}
+hacerBoton();
+
+function ponerBoton(){
+document.body.appendChild(boton);
+}
+ponerBoton();
+
+
+//Función primera tarjeta.
 function tarjetaTrello (){
 
-		if (tarjeta == "" || tarjeta == null || tarjeta == " ") {
-			console.log("No se agrego nada");
-		} else{
-
-			var renglon = document.createElement('div');
-			//Aquí se le da el tipo y la forma al checkbox
-			renglon.setAttribute('class', 'row');
-			//Aquí se crea el texto que formara una lista
-			var tarea = document.createElement('li');
-			//Aquí el checbok se convierte en hijo de lista
-			tarea.appendChild(renglon);
-			//Aquí se crea el texto como hijo de lista
-			tarea.appendChild(document.createTextNode(tarjeta));
-			//Aquí se da ubicación de lista
-			document.body.appendChild(tarea);
-
-
-			//Variable que crea el checkbox
-			var inputBoton = document.createElement('input');
-			//Aquí se le da el tipo y la forma al checkbox
-			inputBoton.setAttribute('type', 'input');
-			//Aquí se crea el texto que formara una lista
-			var contenido = document.createElement('li');
-			//Aquí el checbok se convierte en hijo de lista
-			contenido.appendChild(inputBoton);
-			//Aquí se crea el texto como hijo de lista
-			//Aquí se da ubicación de lista
-			primeraCaja.appendChild(contenido);
-
-			var boton = document.createElement("button");
-			boton.setAttribute("class", "btn");
-			boton.appendChild(document.createTextNode("Guardar"));
-			document.body.appendChild(boton);
-
-		}
+	if ( inputBoton === "" ||  inputBoton === null ||  inputBoton === " ") {
+		alert("No se agrego nada");
+	} else{
+		//Esto hace un div en forma de lista
+		var renglon = document.createElement('div');
+		//Aquí se le da la clase renglón al div
+		renglon.setAttribute('class', 'row');
+		//Aquí se crea el texto que formara una lista
+		var tarea = document.createElement('li');
+		//Aquí el div se convierte en hijo de lista
+		tarea.appendChild(renglon);
+		//Aquí se crea el texto como hijo de lista
+		tarea.appendChild(document.createTextNode(inputBoton.value));
+		//Aquí se da ubicación de lista
+		document.body.appendChild(tarea);
 	}
+	//crear bote de basura
+	var buttonGliphy = document.createElement("button");
+	var spanButtonGliphy = document.createElement("span");
+	spanButtonGliphy.setAttribute("class", "fa fa-trash-o");
+	buttonGliphy.appendChild(spanButtonGliphy);
+	tarea.appendChild(buttonGliphy);
+	document.body.appendChild(tarea);
+
+	//función remover elemento con bote de basura
+	function pulseTrashBox(){
+		tarea.parentNode.removeChild(tarea);
+	}
+	buttonGliphy.onclick = pulseTrashBox;
+}
+
+//On reset inservible!
+//document.getElementsByTagName("input").onreset = function() {clickearBotn()};
+	
+//Función, hacer click al botón
+function clickearBotn(){
+	tarjetaTrello();
+	hacerInput();
+	ponerBoton();
+}
+boton.onclick = clickearBotn;
+
+
 
