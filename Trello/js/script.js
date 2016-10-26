b84b07b7de40a7dd0c0ebc8d74c468b7e723e2eb
TaniaFloresLuna <tanialuna08920@gmail.com>
2016-10-26 13:32:52 -0500

    intento de arreglar el código anterior

--- Trello/js/script.js
+++ Trello/js/script.js
@@ -1,19 +1,13 @@
 
-/*
-tarjeta.onkeypress=function(e){
-	tecla=(tarjeta) ? event.keyCode : e.which;
-	if(tecla==13){
-		var nuevaTarjeta = document.createElement("div");
-		nuevaTarjeta.appendChild(document.createTextNode(tarjeta.value));
-		primeraCaja.appendChild(nuevaTarjeta);
-  	}
-}
-*/
 //variables universales.
-var primeraCaja = document.getElementById("primeraCaja");
-var tarjeta = document.getElementById("tarjeta");
+var primeraCaja = document.createElement("div");
+var tarjeta = document.createElement("textarea");
 var inputBoton = document.createElement('input');
 var boton = document.createElement("button");
+var renglon = document.createElement('div');
+var titulo = document.createElement('h2');
+var primario = document.createElement('li');
+
 
 //función para el input
 function hacerInput(){
@@ -28,62 +22,110 @@ hacerInput();
 
 //Función crear del botón
 function hacerBoton(){
-
-boton.setAttribute("class", "btn");
-boton.appendChild(document.createTextNode("Guardar"));
+	boton.setAttribute("class", "btn");
+	boton.appendChild(document.createTextNode("Guardar"));
 }
 hacerBoton();
 
+//Función para poner el botón
 function ponerBoton(){
-document.body.appendChild(boton);
+	document.body.appendChild(boton);
 }
 ponerBoton();
-
 
 //Función primera tarjeta.
 function tarjetaTrello (){
 
-	if ( inputBoton === "" ||  inputBoton === null ||  inputBoton === " ") {
+	if ( inputBoton.value === "" ||  inputBoton.value === null ||  inputBoton.value === " ") {
 		alert("No se agrego nada");
 	} else{
-		//Esto hace un div en forma de lista
-		var renglon = document.createElement('div');
 		//Aquí se le da la clase renglón al div
 		renglon.setAttribute('class', 'row');
-		//Aquí se crea el texto que formara una lista
-		var tarea = document.createElement('li');
 		//Aquí el div se convierte en hijo de lista
-		tarea.appendChild(renglon);
+		titulo.appendChild(renglon);
 		//Aquí se crea el texto como hijo de lista
-		tarea.appendChild(document.createTextNode(inputBoton.value));
+		titulo.appendChild(document.createTextNode(inputBoton.value));
 		//Aquí se da ubicación de lista
-		document.body.appendChild(tarea);
+		document.body.appendChild(titulo);
 	}
+}	
+
+
+
+function hacerTablero(){
+	//Aquí se crea el texto que formara una lista
+
+	//Aquí el div se convierte en hijo de lista
+	primario.appendChild(renglon);
+	//Aquí se crea el texto como hijo de lista
+	primario.appendChild(document.createTextNode(inputBoton.value));
+	//Aquí se da ubicación de lista
+	document.body.appendChild(primario);
+}
+hacerTablero();
+
+//Función hacer un bote de basurita
 	//crear bote de basura
+function crearTrash(){
 	var buttonGliphy = document.createElement("button");
 	var spanButtonGliphy = document.createElement("span");
 	spanButtonGliphy.setAttribute("class", "fa fa-trash-o");
 	buttonGliphy.appendChild(spanButtonGliphy);
-	tarea.appendChild(buttonGliphy);
-	document.body.appendChild(tarea);
+	primario.appendChild(buttonGliphy);
+	document.body.appendChild(primario);
+
 
 	//función remover elemento con bote de basura
 	function pulseTrashBox(){
-		tarea.parentNode.removeChild(tarea);
+		primario.parentNode.removeChild(primario);
 	}
 	buttonGliphy.onclick = pulseTrashBox;
 }
+
+
+
+function crearsegundoTrash(){
+	var buttonSecondGliphy = document.createElement("button");
+	var spanButtonSecondGliphy = document.createElement("span");
+	spanButtonSecondGliphy.setAttribute("class", "fa fa-trash-o");
+	buttonSecondGliphy.appendChild(spanButtonSecondGliphy);
+	titulo.appendChild(buttonSecondGliphy);
+	document.body.appendChild(titulo);
+
+
+	//función remover elemento con bote de basura
+	function pulseSecondTrashBox(){
+		titulo.parentNode.removeChild(titulo);
+	}
+	buttonSecondGliphy.onclick = pulseSecondTrashBox;
+}
+
+
+//Función que solo hace el titulo y el bote de basura
+function primerTitulo(){
+		tarjetaTrello();
+		crearsegundoTrash();
+}
+
+
 
 //On reset inservible!
 //document.getElementsByTagName("input").onreset = function() {clickearBotn()};
 	
 //Función, hacer click al botón
 function clickearBotn(){
-	tarjetaTrello();
-	hacerInput();
-	ponerBoton();
+	primerTitulo();
+
 }
 boton.onclick = clickearBotn;
 
 
 
+tarjeta.onkeypress=function(e){
+	tecla=(tarjeta) ? event.keyCode : e.which;
+	if(tecla==13){
+	hacerTablero();	
+	crearTrash();
+	hacerInput();
+  	}
+}
